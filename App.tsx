a208eda9be5f0349a7181a04daa77b0560e34721
Yonier Vasquez Marin <yoniervasquezmarin@gmail.com>
2024-07-10 21:57:00 -0500

    Files updated

--- App.tsx
+++ App.tsx
@@ -1,13 +1,10 @@
-import { SafeAreaView } from 'react-native-safe-area-context'
-import { View } from 'react-native'
-import Main from './src/screens/Main'
+import { NavigationContainer } from '@react-navigation/native'
+import DrawerNavigator from './src/navigation/DrawerNavigator'
 
 export default function App() {
     return (
-        <View>
-            <SafeAreaView>
-                <Main />
-            </SafeAreaView>
-        </View>
+        <NavigationContainer>
+            <DrawerNavigator />
+        </NavigationContainer>
     )
 }
---
+++ src/screens/Tutorial.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function Tutorial() {
+    return (
+        <View>
+            <Text>This is the tutorial</Text>
+        </View>
+    )
+}
