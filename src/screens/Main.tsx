9fb72b4c485b8f1c415aed3ee41cc0acb94dcb2c
Yonier Vasquez Marin <yoniervasquezmarin@gmail.com>
2024-07-11 21:23:00 -0500

    Navegación aplicada para ir a otra screen

--- src/screens/Main.tsx
+++ src/screens/Main.tsx
@@ -1,12 +1,24 @@
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, Button } from 'react-native'
+import { useNavigation } from '@react-navigation/native'
 import React from 'react'
+import { StackNavigationProp } from '@react-navigation/stack'
+import { ScreenList } from './ScreenStack'
+
+type MainScreenNavigationProp = StackNavigationProp<ScreenList, 'Main'>
 
 export default function Main() {
+    const navigation = useNavigation<MainScreenNavigationProp>()
+
+    const goToTutorialScreen = () => {
+        navigation.navigate('Tutorial')
+    }
+
     return (
         <View style={styles.container}>
             <Text>
                 Welcome to <Text style={styles.strong}>Main screen</Text>
             </Text>
+            <Button title="Go to tutorial" onPress={goToTutorialScreen} />
         </View>
     )
 }
@@ -14,7 +26,7 @@ export default function Main() {
 const styles = StyleSheet.create({
     container: {
         marginTop: 16,
-        alignItems: 'center'
+        alignItems: 'center',
     },
     strong: {
         fontWeight: 'bold',
---
+++ src/screens/ScreenStack.tsx
@@ -0,0 +1,4 @@
+export type ScreenList = {
+    Main: undefined
+    Tutorial: undefined
+}
